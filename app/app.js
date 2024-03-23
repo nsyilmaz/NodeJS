21961069741190057da1a4997c823f823ee24e4e
nebiyilmaz <nebiyilmaz@nomo.tech>
2024-03-23 16:10:33 +0000

    no message

--- app/app.js
+++ app/app.js
@@ -3,13 +3,13 @@ var mysql = require('mysql2');
 
 
 var con = mysql.createConnection({
-  host: "192.168.1.125",
+  host: "10.127.10.42",
   user: "test",
   password: "test",
   database: "test"
 });
 
-
+console.log("hede");
 
 con.connect(function(err) {
   if (err) throw err;
