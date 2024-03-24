50f54e67970bb5493aefd4974aeb92f3ce9c873c
nebiyilmaz <nebiyilmaz@nomo.tech>
2024-03-24 12:45:46 +0000

    no message

--- cli/db/mysqlclient.sh
+++ cli/db/mysqlclient.sh
@@ -2,6 +2,6 @@
 
 #docker exec -ti mysql /bin/bash
 #docker exec -ti mysql mysql -h 127.0.0.1 -u root -p
-docker exec -ti mysql mysql -u root -p
+docker exec -ti mysql-db mysql -u root -p
 
 
---
+++ web/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+#docker-compose --env-file docker.env  build 
+docker-compose --env-file docker.env  up -d
+
+docker ps -a
+
+
+#
+# MAC uzerinde calisitirinca /var/lib/mysql/mysqld.sock dosyasi link olarak problem oluyor.
+# Bu durum MAC'e ozel.
+#
+
